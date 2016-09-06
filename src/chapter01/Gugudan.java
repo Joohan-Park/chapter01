37950bada7e84d1d7a404e97ed0c3dead8e627ae
joohanpark <wngksqkr91@naver.com>
2016-09-06 17:38:43 +0900

    수업

--- src/chapter01/Gugudan.java
+++ src/chapter01/Gugudan.java
@@ -3,10 +3,9 @@ package chapter01;
 public class Gugudan {
 
 	public static void main(String[] args) {
-		for(int j=1;j<=9;j++){
-			for(int i=1; i<=9;i++){
-				System.out.print(i+"*"+j+"="+(i*j));
-				System.out.print("\t");
+		for (int j = 1; j <= 9; j++) {
+			for (int i = 1; i <= 9; i++) {
+				System.out.print(i + "*" + j + "=" + (i * j) + "\t");
 			}
 			System.out.println();
 		}
--- src/chapter01/Triangle.java
+++ src/chapter01/Triangle.java
@@ -3,17 +3,11 @@ package chapter01;
 public class Triangle {
 
 	public static void main(String[] args) {
-		
-		
-		for(int i=0;i<6;i++){
-			for(int j=0;j<=i;j++){
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j <= i; j++) {
 				System.out.print("X");
 			}
 			System.out.println();
 		}
-		
-		
-
 	}
-
 }
